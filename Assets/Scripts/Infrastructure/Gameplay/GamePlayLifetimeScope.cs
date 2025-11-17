5828d3dd884460c49d7c7c37b5534d235e3a7eae
Max <m.leshchinskiy@gmail.com>
2025-11-17 10:59:48 +0300

    moved scripts to scripts folder

---
+++ Assets/Scripts/Infrastructure/Gameplay/GamePlayLifetimeScope.cs
@@ -0,0 +1,11 @@
+using VContainer;
+using VContainer.Unity;
+
+public class GamePlayLifetimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        
+    }
+    
+}
